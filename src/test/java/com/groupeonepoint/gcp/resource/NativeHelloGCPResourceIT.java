253238c157cdeb43c1306b15bc5fdae2d1ec901d
Jérôme TAMA <j.tama@groupeonepoint.com>
2019-12-06 17:31:44 +0100

    removing java project

--- src/test/java/com/groupeonepoint/gcp/resource/NativeHelloGCPResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.groupeonepoint.gcp.resource;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloGCPResourceIT extends HelloGCPResourceTest {
-
-    // Execute the same tests but in native mode.
-}
